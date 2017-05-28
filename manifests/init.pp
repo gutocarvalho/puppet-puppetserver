6a19dceffb41a5301cb131d1fbfc13c09705f87a
Guto Carvalho <gutocarvalho@instruct.com.br>
2017-05-27 22:44:06 -0300

    Added comments, class ordering

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,16 @@
-# docs
+# Main class
+## Parameters
+### certname: Certificate name for the server and agent
+### version: package version for puppetserver
+### autosign: Set the parameter autosign inside puppet.config
+### agent_version: package version for puppet agent
+### java_args: Set the JAVA_ARGS for puppetserver JVM
+### system_config_path: The path to be used for the system config
+### puppetdb: Configure puppetdb integration
+### puppetdb_version: package version for puppetdb
+### puppetdb_server: host or ip for puppetdb integration
+### puppetdb_port: port of puppetdb service
+
 class puppetserver(
   String $certname,
   String $version,
@@ -16,8 +28,8 @@ class puppetserver(
   include puppetserver::config
   include puppetserver::service
 
-  Class['puppetserver::install'] ->
-  Class['puppetserver::config'] ->
-  Class['puppetserver::service']
+  Class['puppetserver::install']
+  -> Class['puppetserver::config']
+    -> Class['puppetserver::service']
 
 }
