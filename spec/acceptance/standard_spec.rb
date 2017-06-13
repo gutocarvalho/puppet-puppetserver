d92d63acb2a3c3ab4c30a93ac8167eabeb2757ce
Guto Carvalho <gutocarvalho@gmail.com>
2017-06-13 20:44:30 -0300

    Added beaker tests

---
+++ spec/acceptance/standard_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper_acceptance'
+
+describe 'puppetserver class' do
+  context 'should work with no errors' do
+    it 'when configuring puppetserver' do
+      pp = <<-EOS
+        class { 'puppetserver': }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true) do |r|
+        expect(r.stderr).not_to match(/error/i)
+        expect(r.exit_code).to eq(2)
+      end
+
+      apply_manifest(pp, :catch_failures => true) do |r|
+        expect(r.stderr).not_to eq(/error/i)
+        expect(r.exit_code).to be_zero
+      end
+    end
+
+    describe service('puppetserver') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(8140) do
+      it { should be_listening }
+    end
+
+  end
+end
