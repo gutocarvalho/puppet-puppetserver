0f74a3312a63c7acc8743c241589f2a99c59c297
Guto Carvalho <gutocarvalho@gmail.com>
2017-06-15 19:22:20 -0300

    Added puppet string documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,20 @@
-# Main class
-## Parameters
-### certname: Certificate name for the server and agent
-### version: package version for puppetserver
-### autosign: Set the parameter autosign inside puppet.config
-### agent_version: package version for puppet agent
-### java_args: Set the JAVA_ARGS for puppetserver JVM
-### system_config_path: The path to be used for the system config
-### puppetdb: Configure puppetdb integration
-### puppetdb_version: package version for puppetdb
-### puppetdb_server: host or ip for puppetdb integration
-### puppetdb_port: port of puppetdb service
+# Puppetserver calss.
+#
+# This is a class to install and manage Puppetserver:
+#
+# @example Declaring the class
+#   include puppetserver
+#
+# @param [String] certname Certificate name for the server and agent
+# @param [String] version Package version for puppetserver
+# @param [Boolean] autosign Set the parameter autosign inside puppet.conf
+# @param [String] agent_version Package version for puppet agent
+# @param [String] java_args Set the JAVA_ARGS for puppetserver JVM
+# @param [String] system_config_path The path to be used for the system config
+# @param [Boolean] puppetdb Define if puppetdb integration is enabled
+# @param [String] puppetdb_version Package version for puppetdb
+# @param [String] puppetdb_server Host or ip for puppetdv service
+# @param [Integer] puppetdb_port Port to connect for puppetdb service
 
 class puppetserver(
   String $certname,
