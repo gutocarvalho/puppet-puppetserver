4e858136d9952629a1ca9ddde9f5edd60db7149c
Guto Carvalho <gutocarvalho@gmail.com>
2017-07-02 21:05:02 -0300

    Acceptance tests change to support Puppet 5

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,17 +1,14 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
-require 'beaker/puppet_install_helper'
 
-run_puppet_install_helper
+#require 'beaker/puppet_install_helper'
+#run_puppet_install_helper
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
-HIERA_FILE = 'hiera.yaml'
 
 RSpec.configure do |c|
   c.before :suite do
     hosts.each do |host|
-      scp_to(host, HIERA_FILE, "/etc/puppetlabs/puppet/#{HIERA_FILE}" )
       copy_module_to(host, source: PROJECT_ROOT, module_name: 'puppetserver')
     end
   end
