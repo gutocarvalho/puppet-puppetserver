7ab39f04e23518c1c3c171e188f971a654025966
Guto Carvalho <gutocarvalho@instruct.com.br>
2018-01-23 17:22:24 -0200

    Changing beaker invocation command

--- acceptance.sh
+++ acceptance.sh
@@ -1,4 +1,4 @@
 BOXES=( default centos-6-x64 ubuntu-1604-x64 debian-8-x64 )
 for BOX in "${BOXES[@]}"; do
-  PUPPET_INSTALL_TYPE="agent" BEAKER_set="${BOX}" bundle exec rake beaker
+  PUPPET_INSTALL_TYPE="agent" BEAKER_set="${BOX}" rake beaker
 done
